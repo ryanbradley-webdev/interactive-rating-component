946c9d852795a115886e7a54adf41cf7a32ac8b4
Ryan Bradley <ryanbradley315@gmail.com>
2023-08-08 14:33:46 -0500

    Update card styling and page layout.

--- src/App.tsx
+++ src/App.tsx
@@ -2,45 +2,41 @@ import './App.css'
 
 function App() {
   return (
-    <main>
-      
-      <section>
+    <section>
 
-        <h2>
-          How did we do?
-        </h2>
+      <h2>
+        How did we do?
+      </h2>
 
-        <p>
-          Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!
-        </p>
+      <p>
+        Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!
+      </p>
 
-        <div>
+      <div>
 
-          <button>
-            1
-          </button>
+        <button>
+          1
+        </button>
 
-          <button>
-            2
-          </button>
+        <button>
+          2
+        </button>
 
-          <button>
-            3
-          </button>
+        <button>
+          3
+        </button>
 
-          <button>
-            4
-          </button>
+        <button>
+          4
+        </button>
 
-          <button>
-            5
-          </button>
+        <button>
+          5
+        </button>
 
-        </div>
+      </div>
 
-      </section>
-
-    </main>
+    </section>
   )
 }
 
